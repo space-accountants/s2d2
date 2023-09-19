c65ed77cdd2454fa2d6dc14b399decd84480bf50
Bas Altena <b.altena@uu.nl>
2023-09-19 17:05:41 +0200

    Update naming_check.py

--- s2d2/naming_check.py
+++ s2d2/naming_check.py
@@ -8,9 +8,8 @@ __email__ = "info at space hyphen accountants dot eu"
 
 import re
 
-def check_mgrs_code(tile_code):
-    """
-    Validate a MGRS tile code and make it uppercase
+def check_mgrs_code(code_str):
+    """ validate a MGRS tile code and make it uppercase
 
     Parameters
     ----------
@@ -19,7 +18,7 @@ def check_mgrs_code(tile_code):
 
     Returns
     -------
-    tile_code : str
+    code_str : str
         validated and normalized tile code
 
     Notes
@@ -29,12 +28,33 @@ def check_mgrs_code(tile_code):
         - MGRS : US Military Grid Reference System
     """
 
-    if not isinstance(tile_code, str):
+    if not isinstance(code_str, str):
         raise TypeError("please provide a string")
 
-    tile_code = tile_code.upper()
+    tile_code = code_str.upper()
+    pattern = "[0-9][0-9][A-Z][A-Z][A-Z]"
 
-    if not bool(re.match("[0-9][0-9][A-Z][A-Z][A-Z]", tile_code)):
+    if not bool(re.match(pattern, tile_code)):
         raise ValueError("please provide a correct MGRS tile code")
 
     return tile_code
+
+def check_sen2_safe_code(code_str):
+    if not isinstance(code_str, str):
+        raise TypeError("please provide a string")
+
+    code_str = code_str.upper()
+    pattern = "S2A_MSIL1C_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]" + \
+              "T[0-9][0-9][0-9][0-9][0-9][0-9]_" + \
+              "N[0-9][0-9][0-9][0-9]_" + \
+              "R[0-9][0-9][0-9]_" + \
+              "T[0-9][0-9][A-Z][A-Z][A-Z]_" + \
+              "[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]" + \
+              "T[0-9][0-9][0-9][0-9][0-9][0-9].SAFE"
+
+    if not bool(re.match(pattern, code_str)):
+        raise ValueError("please provide a correct Sentinel-2 tile code")
+    return code_str
+
+#todo: def check_sen2_granule_code(code_str):
+#todo: def check_sen2_datastrip_code
