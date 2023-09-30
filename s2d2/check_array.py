e9e8705b3fd79e31c62dc87effbeb6d390ab97d2
Bas Altena <b.altena@uu.nl>
2023-09-30 17:21:17 +0200

    Create check_array.py

---
+++ s2d2/check_array.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def are_two_arrays_equal(A, B):
+    assert type(A) in (np.ma.core.MaskedArray, np.ndarray), \
+        ('please provide an array')
+    assert type(B) in (np.ma.core.MaskedArray, np.ndarray), \
+        ('please provide an array')
+    assert A.ndim==B.ndim, ('please provide arrays of same dimension')
+    assert A.shape==B.shape, ('please provide arrays of equal shape')
+    return
+
+def are_three_arrays_equal(A, B, C):
+    assert type(A) in (np.ma.core.MaskedArray, np.ndarray), \
+        ('please provide an array')
+    assert type(B) in (np.ma.core.MaskedArray, np.ndarray), \
+        ('please provide an array')
+    assert type(C) in (np.ma.core.MaskedArray, np.ndarray), \
+        ('please provide an array')
+    assert len(set({A.shape[0], B.shape[0], C.shape[0]})) == 1, \
+         ('please provide arrays of the same size')
+    if np.logical_and.reduce((A.ndim>1, B.ndim>1, C.ndim>1)):
+        assert len(set({A.shape[1], B.shape[1], C.shape[1]})) == 1, \
+             ('please provide arrays of the same size')
+    assert len(set({A.ndim, B.ndim, C.ndim})) == 1, \
+         ('please provide arrays of the same dimension')
+    return
