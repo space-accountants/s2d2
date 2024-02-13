faa620dbfeee87cf32955e0ee16b6ee1d95ddbd0
Francesco Nattino <f.nattino@esciencecenter.nl>
2024-02-13 14:29:05 +0100

    move setup.cfg content to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-import os
 from setuptools import setup
 
-extras_require={
- "test": ["coverage", "mypy", "pycodestyle", "pytest", "pytest-cov",
- "pytest-mock", "pytest-asyncio"]}
-
-# see setup.cfg
 setup()
